e81937e53e2354b4fb2b64c9cb2d99d5e53c458c
OnlyPassionCode <van.wassenhove.kevin.pro@hotmail.com>
2024-10-17 23:30:39 +0200

    refactor design cart

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -13,11 +13,11 @@ type CartItem = {
 
 function CartItem({item, removeFromCart, incrementItem, decrementItem}: CartItem){
     return (
-    <div className="flex items-center gap-4">
+    <div className="flex items-center justify-center xl:justify-start gap-4">
         <div className="w-36 h-36 shrink-0 bg-white p-2 rounded-md">
             <img src={"/images/resized/webp/" + item.getImageUrl()} className="w-full h-full object-contain select-none pointer-events-none" />
         </div>
-        <div className="w-full">
+        <div className="xl:w-full">
             <h3 className="text-lg font-semibold text-gray-800">{item.getName()}</h3>
             <h6 className="text-md text-gray-800 font-bold cursor-pointer mt-0.5">{item.getTotalPrice()}€</h6>
 
@@ -37,7 +37,7 @@ function CartItem({item, removeFromCart, incrementItem, decrementItem}: CartItem
                     </button>
                 </div>
 
-                <div className="ml-auto" onClick={() => removeFromCart(item)}>
+                <div className="ml-2 xl:ml-auto" onClick={() => removeFromCart(item)}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="w-5 fill-red-500 inline cursor-pointer" viewBox="0 0 24 24">
                         <path d="M19 7a1 1 0 0 0-1 1v11.191A1.92 1.92 0 0 1 15.99 21H8.01A1.92 1.92 0 0 1 6 19.191V8a1 1 0 0 0-2 0v11.191A3.918 3.918 0 0 0 8.01 23h7.98A3.918 3.918 0 0 0 20 19.191V8a1 1 0 0 0-1-1Zm1-3h-4V2a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v2H4a1 1 0 0 0 0 2h16a1 1 0 0 0 0-2ZM10 4V3h4v1Z" data-original="#000000"></path>
                         <path d="M11 17v-7a1 1 0 0 0-2 0v7a1 1 0 0 0 2 0Zm4 0v-7a1 1 0 0 0-2 0v7a1 1 0 0 0 2 0Z" data-original="#000000"></path>
