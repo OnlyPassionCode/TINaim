9698ca24b179eebba8f76fc05dd6e09ffac2af34
OnlyPassionCode <van.wassenhove.kevin.pro@hotmail.com>
2024-10-17 11:49:58 +0200

    catalog tailwind + fix rename image

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -8,15 +8,52 @@ type CatalogItemProps = {
 
 function CatalogItem({item}: CatalogItemProps){
     return (
-    <div>
-        <img src="https://placehold.co/400" alt={item.getName()} />
-        <h2>{item.getName()}</h2>
-        <p>{item.getDescription()}</p>
-        <p>Prix : {item.getPrice()}€</p>
-        <p>Article restant : {item.getAmount()}</p>
-        <button>Detail de l'article</button>
-        <button>Ajouter au panier</button>
-    </div>)
+    <div className="w-[90%] sm:w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 sm:ml-4 mt-4">
+        <a href="#">
+            <img className="p-8 rounded-t-lg w-[400px] h-[400px] w-full h-full object-contain" src={"/images/" + item.getImageUrl()} alt={item.getName()} />
+        </a>
+        <div className="px-5 pb-5">
+            <a href="#">
+                <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{item.getDescription()}</h5>
+            </a>
+            <div className="flex justify-between">
+                <div className="flex items-center mt-2.5 mb-5">
+                    <div className="flex items-center space-x-1 rtl:space-x-reverse">
+                        <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                        </svg>
+                        <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                        </svg>
+                        <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                        </svg>
+                        <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                        </svg>
+                        <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                        </svg>
+                    </div>
+                    <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">5.0</span>
+                </div>
+                <div className="flex items-center mt-2.5 mb-5">
+                    <span className="text-xl tracking-tight text-green-900 dark:text-white">
+                    {item.getAmount() > 0 
+                    ? `${item.getAmount()} article${item.getAmount() > 1 ? "s" : ""} restant` 
+                    : <span className="text-red-900">Plus de stock</span>}
+                    </span>
+                </div>
+            </div>
+            <div className="flex items-center justify-between">
+                <span className="text-3xl font-bold text-gray-900 dark:text-white">{item.getPrice()}€</span>
+                <button className={`${item.getAmount() > 0 ? '' : 'opacity-50 cursor-not-allowed'} text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}
+                        disabled={item.getAmount() <= 0}>
+                            Ajouter au panier
+                </button>
+            </div>
+        </div>
+    </div>);
 }
 
 export default function Catalog(){
@@ -43,7 +80,7 @@ export default function Catalog(){
     }, []);
 
     return (
-        <section id="catalog">
+        <section className="w-full sm:w-[75%] flex justify-center sm:justify-start flex-wrap">
             {items.map((item: Item, index: number)=><CatalogItem key={index} item={item} />)}
         </section>
     )
